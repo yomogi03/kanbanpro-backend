4673f7d9118b66d8e32ed0e332d48e2a34abccda
yomogi03 <yomo.yomo03@gmail.com>
2025-12-06 00:06:33 +0900

    Add .gitignore updates and initial project setup

---
+++ src/main/java/com/example/kanbanpro/KanbanproApplication.java
@@ -0,0 +1,13 @@
+package com.example.kanbanpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanproApplication.class, args);
+	}
+
+}
